bad8c895efdc8ccb8541284a43fa8f5c8e5bbe99
adamsjac <adamsjacques546@gmail.com>
2026-02-03 17:18:59 -0500

    update jedi model with new fields

--- JediOrderRegistry.Api/Models/JediModel.cs
+++ JediOrderRegistry.Api/Models/JediModel.cs
@@ -5,16 +5,20 @@ namespace JediOrderRegistry.Api.Models
     public class JediModel
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Rank { get; set; }
-        public string LightsaberColor { get; set; }
+        required public string Name { get; set; }
+        required public string? Rank { get; set; }
+        public Guid? Lightsaber { get; set; }
         public int MidichlorianCount { get; set; }
-        public string Species { get; set; }
-        public string Homeworld { get; set; }
+        public string? Species { get; set; }
+        public string? Homeworld { get; set; }
         public int Age { get; set; }
         public int YearsOfService { get; set; }
-        public string MasterName { get; set; }
-        public string CurrentAssignment { get; set; }
+        public Guid? Master { get; set; }
+        public Guid? Padawan { get; set; }
+        public Guid? CurrentAssignment { get; set; }
         public bool IsActive { get; set; }
+
+        public JediModel() { }
+
     }
 }
